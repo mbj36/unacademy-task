be7b1c8fb4596657485ecb4774d63147e124575d
mbj36 <mohitbajo36@gmail.com>
2017-12-13 02:48:49 +0530

    Finalised Code

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,37 +1,44 @@
 <template>
-  <div class="center">
-    <h3>
-      Are you ready to start the quiz ?
-    </h3>
-    <br>
-    <br>
-    <router-link to="/quiz">
-      <v-btn class="primary" dark large>
-        Start Quiz
-      </v-btn>
-    </router-link>
 
-  </div>
+    <div class="center">
+
+        <h3>
+            Are you ready to start the quiz ?
+        </h3>
+
+        <router-link to="/quiz">
+            <!-- Ask the User to start the quiz and redirect to quiz page -->
+
+            <v-btn class="primary" dark large>
+
+                Start Quiz
+
+            </v-btn>
+
+        </router-link>
+
+    </div>
 
 </template>
 
 <script>
-  export default {};
+    export default {};
 </script>
 
 <style>
-  .center {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      top: 300px;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      margin: auto;
-  }
-  a,
-  u {
-      text-decoration: none;
-  }
+    /* centering the div */
+    .center {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 300px;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+    }
+    a,
+    u {
+        text-decoration: none;
+    }
 </style>
--- src/components/QuizQuestion.vue
+++ src/components/QuizQuestion.vue
@@ -1,85 +1,107 @@
 <template>
-  <div>
-    <div v-for="(question, index) in questions" :key="question.id">
-      <div v-show="index === questionIndex">
-        <h2>{{ question.text }}</h2>
-        <div v-for="(response,index) in question.options" :key="response.id">
-          <v-btn color="primary" @click="next" :value="question.answer" v-model="userResponse[index]">{{response}}</v-btn>
+
+    <div>
+
+        <div v-for="(question, index) in questions" :key="question.id">
+
+            <div v-show="index === questionIndex">
+
+                <!-- Displaying Text -->
+                <h2>{{ question.text }}</h2>
+
+                <!-- Displaying Options -->
+                <div v-for="(response,index) in question.options" :key="response.id">
+                    <v-btn color="primary" @click="next">{{response}}</v-btn>
+                </div>
+
+                <!-- Skip Button -->
+                <v-btn large class="error" @click="skip">Skip</v-btn>
+
+            </div>
+
         </div>
-        <v-btn large class="error" @click="skip">Skip</v-btn>
-      </div>
+
+        <div v-show="questionIndex === questions.length">
+
+            <h2>Thanks for taking the Quiz</h2>
+
+            <h3>Checkout your scores:</h3>
+
+            <div class="inline">
+
+                <v-flex xs4 ml-5>
+
+                    <v-card dark color="primary">
+                        <v-card-text class="px-0 white--text">Correct Answer</v-card-text>
+                    </v-card><br>
+
+                    <li v-for="(question, index) in questions" :key="question.id">
+                        {{question.answer}}
+                    </li>
+
+                </v-flex>
+
+                <v-flex xs4>
+
+                    <v-card dark color="error">
+                        <v-card-text class="px-0 white--text">Your Answer</v-card-text>
+                    </v-card><br>
+
+                    <li v-for="x in userResponse" :key="x.id">
+                        {{x}}
+                    </li>
+
+                </v-flex>
+            </div>
+        </div>
     </div>
-    <div v-show="questionIndex === questions.length">
-      <h2>Thanks for taking the Quiz</h2>
-      <h3>Below are your scores:</h3>
-      <h3>Total score:{{ score() }}/{{questions.length}}</h3>
-      <div class="inline">
-        <v-flex xs4 ml-5>
-          <v-card dark color="primary">
-            <v-card-text class="px-0 white--text">Correct Answer</v-card-text>
-          </v-card><br>
-          <li v-for="question in questions" :key="question.id">
-            {{question.answer}}
-          </li>
-        </v-flex>
-        <v-flex xs4>
-          <v-card dark color="error">
-            <v-card-text class="px-0 white--text">Your Answer</v-card-text>
-          </v-card><br>
-          <li v-for="question in questions" :key="question.id">
-            {{question.answer}}
-          </li>
-        </v-flex>
-      </div>
-    </div>
-  </div>
 </template>
 
 
 <script>
-  import axios from 'axios';
+    import axios from 'axios';
 
-  export default {
-      data() {
-          return {
-              questions: '',
-              questionIndex: 0,
-              userResponse: Array(8).fill(false)
-          };
-      },
+    export default {
+        data() {
+            return {
+                questions: '',
+                questionIndex: 0,
+                userResponse: [] //For getting User responses
+            };
+        },
 
-      methods: {
-          singleText: function() {
-              axios.get('../../static/data/questions.json').then(response => {
-                  this.questions = response.data;
-                  console.log(this.questions);
-              });
-          },
+        methods: {
+            // Getting JSON data
+            singleText: function() {
+                axios.get('../../static/data/questions.json').then(response => {
+                    this.questions = response.data;
+                    console.log(this.questions);
+                });
+            },
 
-          next: function() {
-              this.questionIndex++;
-          },
+            next: function(e) {
+                this.questionIndex++;
+                var clickedElement = e.target.innerHTML;
+                this.userResponse.push(clickedElement);
+            },
 
-          skip: function() {
-              this.questionIndex++;
-          },
+            skip: function(e) {
+                this.questionIndex++;
+                var clickedElement = e.target.innerHTML;
+                this.userResponse.push(clickedElement);
+            }
+        },
 
-          score: function() {
-              return this.userResponse.filter(function(val) {
-                  return val;
-              }).length;
-          }
-      },
-
-      created: function() {
-          this.singleText();
-      }
-  };
-  // };
+        created: function() {
+            this.singleText();
+        }
+    };
+    // };
 </script>
 
 <style>
-  .inline {
-      display: flex;
-  }
+    .inline {
+        display: flex;
+        margin-left: 15%;
+    }
 </style>
